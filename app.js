697073b5ca33e9f3338eee106fc6ca143ceae764
Raka Gunarto <rakafattah@yahoo.com>
2019-11-02 14:21:41 +0000

    Add CORS

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express()
 
 global.db = sqlite3("./db/POIs.db")
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(require("./src/routes/crowdsourcing-web-app"))
 app.use(require("./src/routes/native-app"))
 
